dba563a06f642546db4068eb90d74d623d1b404e
wregjru <jancmeieu95@gmail.com>
2025-08-29 16:43:21 +0800

    作业完成

--- 25.8.29/25.8.29/源.c
+++ 25.8.29/25.8.29/源.c
@@ -163,42 +163,147 @@
 //	printf("%d", a);
 //	return 0;
 //}
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#define ALL 10
-void set_number(int arr[ALL][ALL])
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#define ALL 10
+//void set_number(int arr[ALL][ALL])
+//{
+//	for (int i = 0; i < ALL; i++)
+//	{
+//		for (int j = 0; j <= i; j++)
+//		{
+//			if ((j == 0) || (i == j))
+//			{
+//				arr[i][j] = 1;
+//			}
+//			else if ((i != 0) && (i != j))
+//			{
+//				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+//			}
+//
+//		}
+//	}
+//}
+//void yanghui(int arr[ALL][ALL])
+//{
+//	for (int i = 0; i < ALL; i++)
+//	{
+//		for (int j = 0; j < i + 1; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[ALL][ALL] = { 0 };
+//	set_number(arr);
+//	yanghui(arr);
+//	return 0;
+//}
+//int add(int x, int y)
+//{
+//	return x + y;
+//}
+//int sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int div(int x, int y)
+//{
+//	return x / y;
+//}
+//
+//void calc(int (*pf)(int, int),int x,int y)
+//{
+//	printf("%d\n", pf(x, y));
+//}
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d %d", &x, &y);
+//	int n = 0;
+//	scanf("%d", &n);
+//    do {
+//        printf("请选择操作 (1:加, 2:减, 3:乘, 4:除, 0:退出): ");
+//        scanf("%d", &n);
+//
+//        switch (n)
+//        {
+//        case 1:
+//            calc(add, x, y);
+//            break;
+//        case 2:
+//            calc(sub, x, y);
+//            break;
+//        case 3:
+//            calc(mul, x, y);
+//            break;
+//        case 4:
+//            calc(div, x, y);
+//            break;
+//        case 0:
+//            printf("程序结束\n");
+//            break;
+//        default:
+//            printf("无效选择，请重新输入\n");
+//            break;
+//        }
+//    } while (n != 0);
+//	
+//	return 0;
+//}
+#include <stdio.h>
+int runnian(int x)
 {
-	for (int i = 0; i < ALL; i++)
-	{
-		for (int j = 0; j <= i; j++)
-		{
-			if ((j == 0) || (i == j))
-			{
-				arr[i][j] = 1;
-			}
-			else if ((i != 0) && (i != j))
-			{
-				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
-			}
+    if (x / 100 == 0)
+    {
+        if (x / 400 == 0)
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        {
+            if (x / 4 == 0)
+                return 1;
+        }
+    }
+    return 0;
+}
+int main() {
+    int a, b;
+    int n = 0;
+    while (scanf("%d %d", &a, &b) != EOF)
+    {
+        if (runnian(a) == 1)
+        {
+            if (b == 2)
+                n = 29;
 
-		}
-	}
-}
-void yanghui(int arr[ALL][ALL])
-{
-	for (int i = 0; i < ALL; i++)
-	{
-		for (int j = 0; j < i + 1; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-}
-int main()
-{
-	int arr[ALL][ALL] = { 0 };
-	set_number(arr);
-	yanghui(arr);
-	return 0;
+        }
+        if (runnian(a) == 0)
+        {
+            if (b == 2)
+                n = 28;
+
+        }
+        if (b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 || b == 12)
+        {
+            n = 31;
+        }
+        if (b == 4 || b == 6 || b == 9 || b == 11)
+        {
+            n = 30;
+        }
+        printf("%d\n", n);
+    }
+    return 0;
 }
